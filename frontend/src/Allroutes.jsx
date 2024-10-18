d06f3ed1b21a2b4e13faf77342dd6850126b991a
KRISH-200806 <bhuvakrish25@gmail.com>
2024-10-18 21:46:24 +0530

    update

---
+++ frontend/src/Allroutes.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import SignIn from './component/auth/login'
+import SignUp from './component/auth/signup';
+import Bloglist from './component/blog/bloglist';
+import BlogPost from './component/blog/blogpost';
+import Editblog from './component/blog/Editblog';
+
+function Allroutes() {
+  return (
+    <div>
+      <Routes>
+        <Route path="/login" element={<SignIn />}></Route>
+        <Route path="/signup" element={<SignUp />}></Route>
+        <Route path="/bloglist" element={<Bloglist />}></Route>
+        <Route path="/blogpost" element={<BlogPost />}></Route>
+        <Route path='/editblog' element={<Editblog/>}></Route>
+      </Routes>
+    </div>
+  );
+}
+
+export default Allroutes
